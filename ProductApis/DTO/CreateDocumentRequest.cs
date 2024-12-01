6135edb1ffdc9ccf7a399b233981840895b8a8b3
bsuprava <sbarikdotnetar1@gmail.com>
2024-12-02 02:06:36 +0530

    Added get, post, put methods for Get,Create and Update Document

---
+++ ProductApis/DTO/CreateDocumentRequest.cs
@@ -0,0 +1,10 @@
+﻿using ProductApis.Models;
+
+namespace ProductApis.DTO
+{
+    public class CreateDocumentRequest
+    {
+        public string Key { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
--- ProductApis/Services/ProductService.cs
+++ ProductApis/Services/ProductService.cs
@@ -1,6 +1,10 @@
-﻿using MongoDB.Bson;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using ProductApis.DbContexts;
+using ProductApis.DTO;
+using ProductApis.Models;
 
 namespace ProductApis.Services
 {
@@ -21,11 +25,29 @@ namespace ProductApis.Services
             return documents;
         }
 
+        //get document by key name
         public List<BsonDocument> GetDocumentsByKey(string keyName)
         {
             var filter = Builders<BsonDocument>.Filter.Exists(keyName);
             var documents = _mongoCollection.Find(filter).ToList();
             return documents;
+        }
+
+        //create new document
+        public async Task CreateDocumentAsync(BsonDocument newDocument)
+        {
+            await _mongoCollection.InsertOneAsync(newDocument);
+        }
+
+        // Update a document
+        public async Task<bool> UpdateDocumentAsync(string id, List<Product> products)
+        {
+            Dictionary<string, List<Product>> keyValuePairs = new Dictionary<string, List<Product>>();
+            keyValuePairs.Add(id, products);
+            var filter = Builders<BsonDocument>.Filter.Exists(id);
+            var result = await _mongoCollection.ReplaceOneAsync(filter, keyValuePairs.ToBsonDocument());
+
+            return result.ModifiedCount > 0;
         }
     }
 }
