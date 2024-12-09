5f3f73a2d104b86a1203bca656465fe2a8d8701c
bsuprava <sbarikdotnetar1@gmail.com>
2024-12-09 20:43:50 +0530

    feat: Added Minimal apis for product get,post,put,delete

---
+++ ProductMinimalApis/Models/Categoryies.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductMinimalApis.Models
+{
+    // Principal (parent)
+    public class Categoryies
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Products> Products { get; set; } = new List<Products>();// Collection navigation containing dependents
+    }
+}
